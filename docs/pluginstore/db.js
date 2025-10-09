f304248b2ae8920be27dbf5b38190cf45035024c
heckthetech <217954684+heckthetech@users.noreply.github.com>
2025-10-09 18:54:22 +0600

    Update db.js

--- docs/pluginstore/db.js
+++ docs/pluginstore/db.js
@@ -1,1 +1,4 @@
-
+const plugins = [
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/limitsadjuster/plugininfo.json",
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/bettertiles/plugininfo.json",
+];
