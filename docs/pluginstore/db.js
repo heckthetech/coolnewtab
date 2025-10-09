7b06759b6a0d30cac93299df9f52a8ae3d29fa0d
heckthetech <217954684+heckthetech@users.noreply.github.com>
2025-10-09 19:16:36 +0600

    Update db.js

--- docs/pluginstore/db.js
+++ docs/pluginstore/db.js
@@ -2,5 +2,7 @@ const plugins = [
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/limitsadjuster/plugininfo.json",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/bettertiles/plugininfo.json",
    "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/datedisplay/plugininfo.json",
+ "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/forceyahoosearchengine/plugininfo.json",
 ];
+
 
