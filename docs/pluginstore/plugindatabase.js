e06d4921637a2d42c5756b04020c4f99956adaa1
heckthetech <217954684+heckthetech@users.noreply.github.com>
2026-05-07 13:12:44 +0600

    Create plugindatabase.js

---
+++ docs/pluginstore/plugindatabase.js
@@ -0,0 +1,16 @@
+//v2
+const plugins = [
+
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/limitsadjuster/",
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/bettertiles/",
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/datedisplay/",
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/edgealignedlayout/",  
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/deeperbgm/"
+
+];
+
+
+
+
+
+
