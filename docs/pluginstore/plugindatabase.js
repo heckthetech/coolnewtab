cdbb1ae317e2dcac3d46964de7eaf14a4685a8b1
heckthetech <217954684+heckthetech@users.noreply.github.com>
2026-05-13 03:06:38 +0600

    Update plugindatabase.js

--- docs/pluginstore/plugindatabase.js
+++ docs/pluginstore/plugindatabase.js
@@ -2,11 +2,11 @@
 const plugins = [
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/limitsadjuster/",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/bettertiles/",
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/flowcenterdock/",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/datedisplay/",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/edgealignedlayout/",  
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/deeperbgm/",  
-  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/rickrollgif/",  
-  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/flowcenterdock/"
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/rickrollgif/"
 ];
 
 
