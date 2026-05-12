70a86c7a590fe02a6da63c7417db6f5ca0fdf2c2
heckthetech <217954684+heckthetech@users.noreply.github.com>
2026-05-13 03:04:24 +0600

    Update plugindatabase.js

--- docs/pluginstore/plugindatabase.js
+++ docs/pluginstore/plugindatabase.js
@@ -1,13 +1,12 @@
 //v2
 const plugins = [
-
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/limitsadjuster/",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/bettertiles/",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/datedisplay/",
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/edgealignedlayout/",  
   "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/deeperbgm/",  
-  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/rickrollgif/"
-
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/rickrollgif/",  
+  "https://heckthetech.github.io/coolnewtab/pluginstore/plugin%20assets/flowcenterdock/"
 ];
 
 
